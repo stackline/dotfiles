87df63c3235c6108a519265780b89524a158ee01
stackline <kyo10itimanben+noreply@gmail.com>
2018-08-19 14:03:52 +0900

    Use python 3.6 since old version ansible does not work with python 3.7

--- installer.sh
+++ installer.sh
@@ -193,8 +193,10 @@ initialize() {
 
   ### initialize Python environment
   pyenv install 2.7.15
-  pyenv install 3.7.0
-  pyenv global 3.7.0 2.7.15
+  # An error occurs when executing old version ansible with python 3.7
+  # ref. https://github.com/ansible/ansible/issues/32816
+  pyenv install 3.6.6
+  pyenv global 3.6.6 2.7.15
 
   # TODO: Install ruby, node before installing packages
   nodenv install 10.1.0
