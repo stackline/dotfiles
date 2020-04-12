90affaaabcfce23a47f05e8ff7594dcdff194617
stackline <stackline@users.noreply.github.com>
2020-04-12 20:31:29 +0900

    Aggregate pry configuration file to XDG_CONFIG_HOME directory

--- installer.sh
+++ installer.sh
@@ -31,7 +31,6 @@ create_symbolic_links() {
   ln -fsv "${dotfiles_root_dir}"/.bashrc ~/.bashrc
   ln -fsv "${dotfiles_root_dir}"/.bashrc ~/.zshrc # Share config file with Bash
   ln -fsv "${dotfiles_root_dir}"/.gemrc ~/.gemrc
-  ln -fsv "${dotfiles_root_dir}"/.pryrc ~/.pryrc
   ln -fsv "${dotfiles_root_dir}"/.tmux.conf ~/.tmux.conf
 
   ### Directories
