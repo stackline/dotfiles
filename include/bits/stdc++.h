98a92a12a337673181964b5b32e9338ba12788bc
stackline <stackline@users.noreply.github.com>
2020-10-19 15:18:37 +0900

    c++: Cosmetic change (alphabetical order by clang-format)

--- include/bits/stdc++.h
+++ include/bits/stdc++.h
@@ -1,19 +1,19 @@
 // ref. https://atcoder.jp/contests/apg4b/tasks/APG4b_ak
-#include <iostream>  // cout, endl, cin
-#include <string>    // string, to_string, stoi
-#include <vector>    // vector
 #include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
-#include <utility>   // pair, make_pair
-#include <tuple>     // tuple, make_tuple
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
 #include <cstdint>   // int64_t, int*_t
 #include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
 #include <map>       // map
+#include <numeric>   // gcd
 #include <queue>     // queue, priority_queue
 #include <set>       // set
 #include <stack>     // stack
-#include <deque>     // deque
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
 #include <unordered_map> // unordered_map
 #include <unordered_set> // unordered_set
-#include <bitset>    // bitset
-#include <cctype>    // isupper, islower, isdigit, toupper, tolower
-#include <numeric>   // gcd
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
