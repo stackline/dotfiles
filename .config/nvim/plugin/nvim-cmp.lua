1ff0cc0731fa33a44921a0edfbf02c8f15325568
stackline <stackline@users.noreply.github.com>
2022-04-01 15:34:51 +0900

    chore(nvim-cmp): modernize settings referencing an official document

--- .config/nvim/plugin/nvim-cmp.lua
+++ .config/nvim/plugin/nvim-cmp.lua
@@ -1,9 +1,7 @@
--- nvim-cmp
--- ref. https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion#nvim-cmp
-
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
+-- ref. https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion#nvim-cmp
 -- luasnip setup
 local luasnip = require 'luasnip'
 
@@ -27,19 +25,19 @@ cmp.setup {
       select = true,
     },
     ['<Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+      if cmp.visible() then
+        cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+        luasnip.expand_or_jump()
       else
         fallback()
       end
     end,
     ['<S-Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
+      if cmp.visible() then
+        cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+        luasnip.jump(-1)
       else
         fallback()
       end
