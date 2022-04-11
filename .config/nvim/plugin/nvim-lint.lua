5d8a7d1779f16c888641d12be25a459eca312df5
stackline <stackline@users.noreply.github.com>
2022-04-11 21:49:18 +0900

    chore(nvim-lint): run linter when opening a file

--- .config/nvim/plugin/nvim-lint.lua
+++ .config/nvim/plugin/nvim-lint.lua
@@ -51,7 +51,10 @@ lint.linters_by_ft = {
 -- Trigger of linting
 vim.cmd("augroup nvim_lint_trigger_of_linting")
 vim.cmd("  autocmd!")
+-- After writing
 vim.cmd("  autocmd BufWritePost * lua require('lint').try_lint()")
+-- After displaying the selected file with ctrl-p
+vim.cmd("  autocmd BufWinEnter * lua require('lint').try_lint()")
 vim.cmd("augroup END")
 
 -- NOTE: Workaround for Ruby linter
