bd8fe88ee74b7e54f1b8adef907643268d97141f
stackline <stackline@users.noreply.github.com>
2022-06-18 14:52:41 +0900

    fix(nvim-lint): remove the custom linter setting, since it was merged into the plugin

--- .config/nvim/plugin/nvim-lint.lua
+++ .config/nvim/plugin/nvim-lint.lua
@@ -4,40 +4,6 @@ end
 
 local lint = require('lint')
 
--- Custom linters
-lint.linters.rubocop = {
-  cmd = 'rubocop',
-  stdin = true,
-  args = {'--format', 'json', '--force-exclusion', '--stdin', '%:p'},
-  ignore_exitcode = true, -- Rubocop returns 1 as a return code when detecting offenses.
-  parser = function(output)
-    local diagnostics = {}
-    local decoded = vim.json.decode(output)
-    local offences = decoded.files[1].offenses
-
-    local sev = vim.diagnostic.severity
-
-    for _, off in pairs(offences or {}) do
-      table.insert(diagnostics, {
-        lnum = off.location.start_line - 1,
-        col = off.location.start_column - 1,
-        end_lnum = off.location.last_line - 1,
-        end_col = off.location.last_column,
-        severity = (off.severity == "error" and sev.ERROR or sev.WARN ),
-        message = off.message,
-        code = off.cop_name,
-        user_data = {
-          lsp = {
-            code = off.cop_name,
-          }
-        }
-      })
-    end
-
-    return diagnostics
-  end
-}
-
 -- Mapping of filetype and linters
 lint.linters_by_ft = {
   dockerfile = { 'hadolint' },
