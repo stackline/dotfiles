cc2da12e2e8d99efb44ff80aeb5ac3941a012cd1
stackline <stackline@users.noreply.github.com>
2022-06-25 21:30:33 +0900

    feat(man): make the manual for Bash built-in command easier to read

--- .config/bash/man.sh
+++ .config/bash/man.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
-# Colorize manual pages
-#
-#   mb = start blink
-#   md = start bold
-#   me = turn off blink and bold
-#
-#   so = start standout
-#   se = stop standout
-#
-#   us = start underline
-#   ue = stop underline
-#
-#   31 = red
-#   32 = green
-#   33 = yellow
-#
+function help::manpage_format() {
+  ### Build header
+  # Left and right part
+  name=$(echo -n "$@" | tr '[:lower:]' '[:upper:]')
+  section='(1)'
+  manual_page="${name}${section}"
+
+  # Center part
+  manual_type='Builtin Commands Manual'
+
+  # Space part
+  current_terminal_width=$(tput cols)
+  # TODO: 25 is an appropriately determined number.
+  # To flexibly decide the layout, change the calculation method.
+  half_width=$((current_terminal_width / 2 - 25))
+  spaces=$(printf "%${half_width}s")
+
+  header="${manual_page}${spaces}${manual_type}${spaces}${manual_page}"
+
+  ### Build manual
+  manual="${header}\n\n$(help -m "$@")"
+
+  echo -e "${manual}" | bat -l man -p
+}
+
+# Display the manual of Bash built-in command with "help" command.
 function man() {
-  LESS_TERMCAP_mb=$'\e[01;32m' \
-    LESS_TERMCAP_md=$'\e[01;32m' \
-    LESS_TERMCAP_me=$'\e[0m' \
-    LESS_TERMCAP_so=$'\e[01;33m' \
-    LESS_TERMCAP_se=$'\e[0m' \
-    LESS_TERMCAP_us=$'\e[04;31m' \
-    LESS_TERMCAP_ue=$'\e[0m' \
-    command man "$@"
+  manual_file_name=$(command man -w "$@" 2>/dev/null | xargs basename)
+
+  if [ "$manual_file_name" == 'builtin.1' ]; then
+    help::manpage_format "$@"
+  else
+    # ref. https://github.com/sharkdp/bat#man
+    MANPAGER="sh -c 'col -bx | bat -l man -p'" \
+      command man "$@"
+  fi
 }
