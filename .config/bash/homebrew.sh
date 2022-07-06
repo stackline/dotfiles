be4d88bbe193e97b3c25c55095c406d5a69f0d78
stackline <stackline@users.noreply.github.com>
2022-07-06 22:42:50 +0900

    feat(homebrew): disable repeated hints

--- .config/bash/homebrew.sh
+++ .config/bash/homebrew.sh
@@ -5,3 +5,5 @@ export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK=1
 # Do not generate Brewfile.lock.json
 export HOMEBREW_BUNDLE_NO_LOCK=1
+# Do not print any hints.
+export HOMEBREW_NO_ENV_HINTS=1
