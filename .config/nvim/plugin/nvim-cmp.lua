f175a0deede590c5b1738422c4feefad2d2f82fe
stackline <stackline@users.noreply.github.com>
2022-07-12 23:59:12 +0900

    fix(nvim-cmp): change key mapping to vim style

--- .config/nvim/plugin/nvim-cmp.lua
+++ .config/nvim/plugin/nvim-cmp.lua
@@ -30,7 +30,7 @@ cmp.setup {
     -- just like when clicking <Down> key.
     ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
     ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
