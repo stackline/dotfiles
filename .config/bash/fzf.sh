cf5cf3f2b37d94466b9d6b6b2be5e8a6ce823d33
stackline <stackline@users.noreply.github.com>
2022-07-15 11:42:53 +0900

    refactor(fzf): manage fzf settings in a separate file

---
+++ .config/bash/fzf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# --no-unicode
+#   Prevent layout collapse on Vim due to the display of East Asian
+#   Ambiguous Width characters.
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border --no-unicode'
