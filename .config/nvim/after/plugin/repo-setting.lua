58b44da802cb561929447acfc57cae0b52420b5d
stackline <stackline@users.noreply.github.com>
2022-07-18 00:07:54 +0900

    feat(neovim): check if git rev-parse command is executable

--- .config/nvim/after/plugin/repo-setting.lua
+++ .config/nvim/after/plugin/repo-setting.lua
@@ -1,3 +1,23 @@
+local function git_rev_parse_is_executable()
+  local exit_status = os.execute("git rev-parse")
+  if exit_status == 0 then
+    return true
+  else
+    return false
+  end
+end
+
+local function build_repo_setting_file_path()
+  local handle = io.popen("git rev-parse --show-toplevel")
+  local result = handle:read("a")
+  handle:close()
+
+  local repo_toplevel_dir = result:gsub("\n*", "")
+  local repo_setting_file_path = repo_toplevel_dir .. "/.nvim/init.lua"
+
+  return repo_setting_file_path
+end
+
 local function file_exists(filename)
   local handle = io.open(filename, "r")
   if handle ~= nil then
@@ -8,19 +28,22 @@ local function file_exists(filename)
   end
 end
 
+-- TODO: Add display control for verbose messages.
 -- ref. https://code.visualstudio.com/docs/editor/workspaces#_workspace-settings
--- TODO: Check if git rev-parse command is executable.
-local function load_repo_setting()
-  local handle = io.popen("git rev-parse --show-toplevel")
-  local result = handle:read("a")
-  handle:close()
-
-  local repo_toplevel_dir = result:gsub("\n*", "")
-  local repo_setting = repo_toplevel_dir .. "/.nvim/init.lua"
-
-  if (file_exists(repo_setting)) then
-    dofile(repo_setting)
+local function load_repo_setting_file()
+  if git_rev_parse_is_executable() == false then
+    -- print("repo-setting: git rev-parse is not executable.")
+    return
   end
+
+  local repo_setting_file_path = build_repo_setting_file_path()
+
+  if file_exists(repo_setting_file_path) == false then
+    -- print("repo-setting: Setting file does not exist.")
+    return
+  end
+
+  dofile(repo_setting_file_path)
 end
 
-load_repo_setting()
+load_repo_setting_file()
