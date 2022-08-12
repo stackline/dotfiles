04e997e2d9c0057b3fb21519389c52039841cf03
stackline <stackline@users.noreply.github.com>
2022-08-12 10:52:40 +0900

    fix(fzf): also remove the --no-unicode option as neovim no longer requires ambiwidth=double

--- .config/bash/fzf.sh
+++ .config/bash/fzf.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# --no-unicode
-#   Prevent layout collapse on Vim due to the display of East Asian
-#   Ambiguous Width characters.
-export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border --no-unicode'
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border=rounded'
