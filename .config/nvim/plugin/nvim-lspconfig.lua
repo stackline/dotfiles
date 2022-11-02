574a6803e742619e67693ce91d2954f09e9587cf
stackline <stackline@users.noreply.github.com>
2022-11-02 16:55:15 +0900

    feat(neovim): don't use deprecated API

--- .config/nvim/plugin/nvim-lspconfig.lua
+++ .config/nvim/plugin/nvim-lspconfig.lua
@@ -43,7 +43,7 @@ local on_attach = function(_, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 
   -- Show line diagnostics automatically in hover window
   -- ref. https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#show-line-diagnostics-automatically-in-hover-window
