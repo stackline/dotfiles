7592f8e93ead678e50e2c0d42b0eebb68bb895f6
stackline <stackline@users.noreply.github.com>
2022-12-25 13:38:28 +0900

    feat(neovim): execute formatter asynchronously

--- .config/nvim/plugin/nvim-lspconfig.lua
+++ .config/nvim/plugin/nvim-lspconfig.lua
@@ -84,7 +84,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 -- Formatting on save
 vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function()
-    vim.lsp.buf.format()
+    vim.lsp.buf.format({ async = true })
   end,
 })
 
