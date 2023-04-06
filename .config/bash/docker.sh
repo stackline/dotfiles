ae4f205b54e4b819ef2442846483b7312265fcea
stackline <stackline@users.noreply.github.com>
2023-04-06 14:01:10 +0900

    fix(docker): added necessary action

--- .config/bash/docker.sh
+++ .config/bash/docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # NOTE: From 4.18, docker desktop for mac installs symbolic links under home.
+#
+# TODO: You have to manually create a symbolic link to the docker socket created
+# under home directory.
+#
 # ref. https://docs.docker.com/desktop/mac/permission-requirements/#installing-symlinks
 export PATH=$PATH:$HOME/.docker/bin
