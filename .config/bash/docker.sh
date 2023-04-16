fd54a498b1e43a3868dea238f6f76bcfa3a7ac1b
stackline <stackline@users.noreply.github.com>
2023-04-16 14:07:01 +0900

    fix(docker): allow docker to run without manual configuration even after restarting macos

--- .config/bash/docker.sh
+++ .config/bash/docker.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # NOTE: From 4.18, docker desktop for mac installs symbolic links under home.
-#
-# TODO: You have to manually create a symbolic link to the docker socket created
-# under home directory.
-#
 # ref. https://docs.docker.com/desktop/mac/permission-requirements/#installing-symlinks
 export PATH=$PATH:$HOME/.docker/bin
+
+# NOTE: The default value for DOCKER_HOST is "unix:///var/run/docker.sock".
+# /var/run/docker.sock is deleted when macos is restarted.
+# Therefore, after restarting macos, it is necessary to do one of the following.
+#
+# (a) Manually create a symbolic link.
+# (b) Specify the docker socket path in DOCKER_HOST.
+#
+export DOCKER_HOST=unix://$HOME/.docker/run/docker.sock
