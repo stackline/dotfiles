ac0162dccfbcadf1a5485dbd3c9f7b27f875476f
stackline <stackline@users.noreply.github.com>
2024-01-05 23:11:33 +0900

    fix(neovim): Remove unnecessary setting because the default color is fine

--- .config/nvim/plugin/nightfox.lua
+++ .config/nvim/plugin/nightfox.lua
@@ -21,9 +21,6 @@ local groups = {
     -- Make the color of the window separator line easier to see.
     -- ref. https://github.com/EdenEast/nightfox.nvim/blob/e2f961859cbfb2ba38147dc59fdd2314992c8b62/lua/nightfox/group/editor.lua#L26
     VertSplit = { fg = 'palette.blue' },
-    -- Specify the same color as the background color of NormalFloat.
-    -- "bg0" is darker bg.
-    FloatBorder = { bg = 'palette.bg0' },
   },
 }
 
