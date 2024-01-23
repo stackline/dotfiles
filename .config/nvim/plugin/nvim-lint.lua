2c88c5627b84e6ee837309b841eb0015b94c1e77
stackline <stackline@users.noreply.github.com>
2024-01-23 23:14:48 +0900

    fix(nvim-lint): specify an unused variable

--- .config/nvim/plugin/nvim-lint.lua
+++ .config/nvim/plugin/nvim-lint.lua
@@ -15,7 +15,7 @@ local shellcheck = require('lint.linters.shellcheck')
 local shellcheck_builtin_parser = shellcheck.parser
 shellcheck.parser = function(output)
   local diagnostics = shellcheck_builtin_parser(output)
-  for i, value in ipairs(diagnostics) do
+  for i, _ in ipairs(diagnostics) do
     diagnostics[i].message = 'SC' .. diagnostics[i].code .. ': ' .. diagnostics[i].message
   end
   return diagnostics
