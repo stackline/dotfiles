c59e52aedbe1f8776d323f443f612feae2b02922
stackline <stackline@users.noreply.github.com>
2024-01-25 08:33:51 +0900

    fix(nightfox): if I use lsp_signature.nvim, enable this setting

--- .config/nvim/plugin/nightfox.lua
+++ .config/nvim/plugin/nightfox.lua
@@ -21,6 +21,9 @@ local groups = {
     -- Make the color of the window separator line easier to see.
     WinSeparator = { fg = 'palette.blue' },
     FloatBorder = { fg = 'fg0' }, -- default: fg = 'fg3'
+    -- This setting makes the background color of the funciton signature
+    -- displayed in lsp_signature.nvim the same as the background color of the editor.
+    -- NormalFloat = { bg = 'bg1' }, -- default: bg = 'bg0'
   },
 }
 
