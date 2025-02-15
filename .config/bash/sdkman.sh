47542a3e30cb43f9d433b341cbc6476b45d7079a
stackline <stackline@users.noreply.github.com>
2025-02-16 01:00:16 +0900

    fix: to reduce startup time, enable sdkman only when necessary

--- .config/bash/sdkman.sh
+++ .config/bash/sdkman.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# note: To reduce startup time, comment out only when necessary.
+#
 # ref. https://github.com/sdkman/homebrew-tap#installation
-export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
-[[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+# export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
+# [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
