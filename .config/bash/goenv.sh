ccbf888e59aaaba9866acd4b5631fe4bb5ec3289
stackline <stackline@users.noreply.github.com>
2025-07-21 14:27:41 +0900

    fix: enable settings only if goenv is installed

--- .config/bash/goenv.sh
+++ .config/bash/goenv.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ -z "${HOMEBREW_CELLAR}" || ! -d "${HOMEBREW_CELLAR}/goenv" ]]; then
+  return
+fi
+
 export GOENV_ROOT="${XDG_DATA_HOME}/goenv"
 
 if command -v goenv >/dev/null 2>&1; then
