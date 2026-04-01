74519a205e1e0a7bb19847db9985b52045c9ffa1
stackline <stackline@users.noreply.github.com>
2026-04-01 22:20:51 +0900

    feat: enable list option to show tabs and trailing spaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -258,7 +258,7 @@ vim.keymap.set('n', 'tp', ':tabprevious<CR>', { silent = true })
 vim.keymap.set('n', '<Space>', '<Nop>', {})
 
 vim.opt.number = true
-vim.opt.list = false          -- Don't display tab as character
+vim.opt.list = true           -- (default off) Show tabs, trailing spaces, non-breakable space characters
 vim.opt.wrap = false
 vim.opt.fixeol = false
 vim.opt.incsearch = true
